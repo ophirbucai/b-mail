e257192b9a71a50eb601e0025f67cae718e4fa0c
Ophir Bucai <ophirbucai@gmail.com>
2023-12-11 22:27:00 +0200

    On remove email adds removedAt rather than permanently deleting it,  modify mail details layout from a modal to render instead of the mail list

--- src/cmps/MailDetails/MailDetails.jsx
+++ src/cmps/MailDetails/MailDetails.jsx
@@ -6,14 +6,14 @@ import { LucideForward, LucideMail, LucideMailOpen, LucideReply, LucideTrash2 } 
 import { mailService } from "../../services/mailService.js";
 import dayjs from "dayjs";
 import { MailStarButton } from "../MailStarButton/MailStarButton.jsx";
-import { useDelayedClose } from "../../hooks/useDelayedClose.jsx";
+import { useUrl } from "../../hooks/useUrl.jsx";
 
 export function MailDetails() {
-    const [modalRef, onDelayedClose] = useDelayedClose(() => navigate(-1))
     const navigate = useNavigate()
     const [mail, setMail] = useState(null)
     const { mailId } = useParams()
-    const { removeMail, updateMail } = useMailContext()
+    const { getUrl } = useUrl()
+    const { updateMail } = useMailContext()
 
     useEffect(() => {
         async function loadMail() {
@@ -30,10 +30,10 @@ export function MailDetails() {
     }, [mailId])
 
 
-    async function onDelMail() {
+    async function onRemoveMail() {
         try {
-            await mailService.remove(mail.id);
-            removeMail(mail.id);
+            const updatedMail = await mailService.save({ ...mail, deletedAt: Date.now(), isRead: true });
+            await updateMail(updatedMail)
             navigate(-1);
         } catch (err) {
             console.log(err);
@@ -53,38 +53,31 @@ export function MailDetails() {
     return (
         <div className="mail-details">
             {!mail ? <Loading /> : (
-                <>
-                    {(
-                        <div className="mail-details-container" ref={modalRef}>
-                            <div className="mail-details-header flex space-between bottom-divider">
-                                <button onClick={onDelayedClose} className="simple-button"><span>←</span>{" "}<span>Back</span></button>
-                                <div className="flex">
-                                    <button onClick={console.log} disabled className="simple-button tooltip p10"><LucideReply size="1.2em" /><span role="tooltip" hidden>Reply</span></button>
-                                    <button onClick={console.log} disabled className="simple-button tooltip p10"><LucideForward size="1.2em" /><span role="tooltip" hidden>Forward</span></button>
-                                    <button onClick={onDelMail} className="simple-button tooltip p10"><LucideTrash2 size="1.2em" /><span role="tooltip" hidden>Delete</span></button>
-                                    <button onClick={onMarkMailAsUnread} className="simple-button tooltip p10">{mail.isRead ? <LucideMailOpen size="1.1em" /> : <LucideMail size="1.1em" style={{ translate: "0 0.1em" }} />}<span role="tooltip" hidden>Mark As {mail.isRead ? "Unread" : "Read"}</span></button>
-                                    <MailStarButton mail={mail} onStarMail={(updatedMail) => setMail(updatedMail)} />
-                                </div>
+                <div className="mail-details-container">
+                    <div className="mail-details-header">
+                        <button onClick={() => navigate(getUrl("/mail"))} className="back-button simple-button"><span>←</span>{" "}<span>Back</span></button>
+                        <button onClick={console.log} disabled className="simple-button tooltip p10"><LucideReply size="1.2em" /><span role="tooltip" hidden>Reply</span></button>
+                        <button onClick={console.log} disabled className="simple-button tooltip p10"><LucideForward size="1.2em" /><span role="tooltip" hidden>Forward</span></button>
+                        <button onClick={onRemoveMail} className="simple-button tooltip p10"><LucideTrash2 size="1.2em" /><span role="tooltip" hidden>Delete</span></button>
+                        <button onClick={onMarkMailAsUnread} className="simple-button tooltip p10">{mail.isRead ? <LucideMailOpen size="1.1em" /> : <LucideMail size="1.1em" style={{ translate: "0 0.1em" }} />}<span role="tooltip" hidden>Mark As {mail.isRead ? "Unread" : "Read"}</span></button>
+                        <MailStarButton mail={mail} onStarMail={(updatedMail) => setMail(updatedMail)} />
+                    </div>
+                    <div className="mail-details-body p10">
+                        <div className="mail-details-body-header flex column gap5">
+                            <div className="flex space-between">
+                                <span>To: {mail.to}</span>
+                                <small><strong>Author:</strong> {mail.from}</small>
                             </div>
-                            <div className="mail-details-body p10">
-                                <div className="mail-details-body-header flex column gap5">
-                                    <div className="flex space-between">
-                                        <span>To: {mail.to}</span>
-                                        <small><strong>Author:</strong> {mail.from}</small>
-                                    </div>
-                                    <div className="flex space-between">
-                                        <h3><strong>Subject: {mail.subject}</strong></h3>
-                                        <small>{dayjs(mail.sentAt).format("MMMM DD, YYYY [@] H:mm A")}</small>
-                                    </div>
-                                </div>
-                                <div className="mail-details-body-content">
-                                    {mail.body}
-                                </div>
+                            <div className="flex space-between">
+                                <h3><strong>Subject: {mail.subject}</strong></h3>
+                                <small>{dayjs(mail.sentAt).format("MMMM DD, YYYY [@] H:mm A")}</small>
                             </div>
                         </div>
-                    )}
-                    <button onClick={onDelayedClose} className="mail-details-overlay" aria-label="Back to mail"></button>
-                </>
+                        <div className="mail-details-body-content">
+                            {mail.body}
+                        </div>
+                    </div>
+                </div>
             )}
         </div>
     )
